44b79d087db583523263290aa98d013bd05f5d6c
Aleksey <lesha.leshka@inbox.ru>
2018-12-03 16:43:19 +0300

    Validation of form in store method

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -25,7 +25,10 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        Project::create(request(['title', 'description']));
+        Project::create(request()->validate([
+            'title' => 'required|min:3',
+            'description' => 'required|min:3'
+        ]));
 
         return redirect('/projects');
     }
