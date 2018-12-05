3e6b7961c7e4847fb84469d85944d9463cd8985e
Aleksey <lesha.leshka@inbox.ru>
2018-12-06 00:43:51 +0300

    Create new project tasks

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Task;
+use App\Project;
 
 class ProjectTasksController extends Controller
 {
+    public function store(Project $project)
+    {
+        $project->addTask(
+            request()->validate(['description' => 'required'])
+        );
+
+        return back();
+    }
+
     public function update(Task $task)
     {
         $task->update([
