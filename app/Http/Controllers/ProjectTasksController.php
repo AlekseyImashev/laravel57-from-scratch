1905be7afa3dfb9956f19ddceede43bc89c27c43
Aleksey <lesha.leshka@inbox.ru>
2018-12-06 14:53:20 +0300

    Created a new controller for completed/incompleted tasks

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Task;
 use App\Project;
 
 class ProjectTasksController extends Controller
@@ -12,15 +11,6 @@ class ProjectTasksController extends Controller
         $project->addTask(
             request()->validate(['description' => 'required'])
         );
-
-        return back();
-    }
-
-    public function update(Task $task)
-    {
-        $method = request()->has('completed') ? 'complete' : 'incomplete';
-        
-        $task->$method();
 
         return back();
     }
